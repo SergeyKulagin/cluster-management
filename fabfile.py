7c7e649bd81b05760ecf1f778e5407afd2cad886
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-02-10 23:01:52 +0300

    initial cluster management config

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,9 @@ from fabric.api import *
 import config as cfg
 
 env.hosts = [
-    'ubuntu@192.168.100.3',
     'ubuntu@192.168.100.4',
-    'ubuntu@192.168.100.5'
+    'ubuntu@192.168.100.6',
+    'ubuntu@192.168.100.7'
 ]
 
 env.password = cfg.secure['password']
@@ -13,11 +13,16 @@ env.password = cfg.secure['password']
 def cmd(command):
     sudo(command)
 
-
+@parallel
 def pi_ppa():
     cmd("add-apt-repository ppa:ubuntu-raspi2/ppa")
     cmd("apt-get update")
 
+@parallel
+def install_util():
+    cmd("apt-get install stress")
+
+@parallel
 def stress():
     cmd("stress --cpu 4")
 
